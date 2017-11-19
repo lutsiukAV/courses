1eff461deb91f873da76131c921859fe219c2663
Konstantin Dymov <dymov_k_v@ukr.net>
2017-11-19 21:25:31 +0200

    Added REST urls

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from courses_site import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^courses/', include('courses_site.urls')),
@@ -19,4 +20,21 @@ urlpatterns = [
     url(r'^recommend/', views.recommendations),
     url(r'^regenerate/', views.regenerate),
     url(r'^questionnaire/', views.loadInfo),
+    url(r'^user_account/', views.user_account),
 ]
+
+api_urls = [
+    url(r'^accounts/$', views.AccountList.as_view()),
+    url(r'^accounts/(?P<pk>[0-9]+)/$', views.AccountDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^courses_json/$', views.CourseList.as_view()),
+    url(r'^courses_json/(?P<pk>[0-9]+)/$', views.CourseDetail.as_view()),
+    url(r'^questionnaire_json/$', views.QuestionnaireList.as_view()),
+    url(r'^questionnaire_json/(?P<pk>[0-9]+)/$', views.QuestionnaireDetail.as_view()),
+
+]
+
+api_urls = format_suffix_patterns(api_urls)
+
+urlpatterns += api_urls
